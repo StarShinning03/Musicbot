a31573cd6ee9f2dd5d1ff19ce1afc3a451c3ec21
StarShinning03 <132030548+StarShinning03@users.noreply.github.com>
2023-05-12 09:25:17 +0530

    __main__.py

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -58,7 +58,7 @@ async def fallen_startup():
     try:
         await app.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+            f"⚡️ ғʀᴇɴᴢʏ ᴍᴜsɪᴄ ʙᴏᴛ ⚡️\n\n✦ ɪᴅ : `{BOT_ID}`\n✦ ɴᴀᴍᴇ : {BOT_NAME}\n✦ ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
@@ -68,7 +68,7 @@ async def fallen_startup():
     try:
         await app2.send_message(
             SUNAME,
-            f"✯ ғᴀʟʟᴇɴ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+            f"⚡️ ғʀᴇɴᴢʏ ᴍᴜsɪᴄ ᴀss ⚡️\n\n✦ ɪᴅ : `{ASS_ID}`\n✦ ɴᴀᴍᴇ : {ASS_NAME}\n✦ ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
         )
     except:
         LOGGER.error(
